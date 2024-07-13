f51a92bef53c1acbd1a1ae09101ebf066e051e8c
Virtuouz <giovanni_plascencia@hotmail.com>
2024-07-13 17:35:20 -0600

    set up project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,md}"],
+    theme: {
+      extend: {
+        colors: {},
+      },
+    },
+
+    plugins: [],
+  };
+  
