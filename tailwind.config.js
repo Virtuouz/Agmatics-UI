03e910b5ca9e832fcf41a35fb3b113ead4a919b2
Virtuouz <giovanni_plascencia@hotmail.com>
2024-07-14 10:13:05 -0600

    start styling tool calls

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     content: ["./src/**/*.{html,js,md}"],
     theme: {
       extend: {
-        colors: {},
+        colors: {
+            agmatics:{
+                usertext: "#3E3733",
+                aitext: "#2F1F08",
+                promptarea: "#363331"
+            }
+        },
       },
     },
 
